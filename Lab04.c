b59f877b278e8a3ac99ef52a3e073ad3ff041fec
PC <PC@DESKTOP-8MN65CJ>
2025-10-04 18:53:03 +0300

    Добавьте файлы проекта.

---
+++ Lab04.c
@@ -0,0 +1,20 @@
+﻿#include <stdio.h>
+#include <locale.h>
+
+int main()
+{
+    setlocale(LC_ALL, "RUS");
+    int A, B, C;
+    int condition;
+
+    printf("=== ПРОВЕРКА ГИПОТЕЗЫ ===\n");
+    printf("Введите три целых числа (A, B, C): ");
+
+    scanf("%d %d %d", &A, &B, &C);
+
+    condition = (A % 3 == 0) && (B % 3 == 0) && (C % 3 == 0);
+
+    printf("Гипотеза подтверждена (1 - да, 0 - нет): %d\n", condition);
+
+    return 0;
+}
